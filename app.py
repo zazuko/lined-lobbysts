483aa8edabfa4f4cd40d86d1666ba28e04781bb5
magdalena@surowka.ch <magdalena@surowka.ch>
2020-12-15 18:44:23 +0100

    dash

--- app.py
+++ app.py
@@ -9,49 +9,111 @@ import networkx as nx
 import json
 
 client = SparqlClient()
-query = """
-    PREFIX schema: <http://schema.org/>
-    PREFIX dbo: <http://dbpedia.org/ontology/>
+def generate_politician_party_relation():
+    query = """
+        PREFIX schema: <http://schema.org/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
 
-    SELECT ?politician ?party WHERE {
+        SELECT ?politician ?party WHERE {
 
-    ?politicianUri schema:memberOf ?partyUri .
-    ?partyUri <http://www.w3.org/2004/02/skos/core#altLabel> ?party .
-    ?partyUri a dbo:PoliticalParty .
+        ?politicianUri schema:memberOf ?partyUri .
+        ?partyUri <http://www.w3.org/2004/02/skos/core#altLabel> ?party .
+        ?partyUri a dbo:PoliticalParty .
 
-    ?politicianUri schema:givenName ?pGivenName .
-    ?politicianUri schema:familyName ?pFamilyName .
-    BIND(CONCAT(?pGivenName, " ", ?pFamilyName) AS ?politician) .
+        ?politicianUri schema:givenName ?pGivenName .
+        ?politicianUri schema:familyName ?pFamilyName .
+        BIND(CONCAT(?pGivenName, " ", ?pFamilyName) AS ?politician) .
 
-    FILTER(LANG(?party) = "de")
-    }
-"""
+        FILTER(LANG(?party) = "de")
+        }
+    """
 
-df = client.send_query(query)
+    df = client.send_query(query)
+    rows_to_take = (df["party"].value_counts() > 10) #& (df["party"].value_counts() < 20)
+    relevant_orgs = set(rows_to_take[rows_to_take].index)
+    df = df[df['party'].isin(relevant_orgs)]
 
+    elements = []
+    for iterator, group in zip([set(df["party"]), df["politician"]], ["party", "politician"]):
+        for node in iterator:
+            item = {
+                "data" : {
+                    "id": node,
+                    "label": node
+                    },
+                "classes": group
+            }
+            elements.append(item)
 
-elements = []
-for iterator, group in zip([set(df["party"]), df["politician"]], ["party", "politician"]):
-    for node in iterator:
+    for source, target in zip(df["party"], df["politician"]):
         item = {
-            "data" : {
-                "id": node,
-                "label": node
-                },
-            "classes": group
+            "data": {
+                "source": source,
+                "target": target
+            }
         }
         elements.append(item)
 
-for source, target in zip(df["party"], df["politician"]):
-    item = {
-        "data": {
-            "source": source,
-            "target": target
+    return elements
+
+def generate_politician_org_relation():
+    query = """
+        PREFIX schema: <http://schema.org/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX lw: <https://lod.lobbywatch.ch/>
+        PREFIX org: <http://www.w3.org/ns/org#>
+
+        SELECT ?politician ?org WHERE {
+
+        ?politicianUri schema:memberOf ?partyUri .
+        ?partyUri a dbo:PoliticalParty .
+        ?politicianUri schema:givenName ?pGivenName .
+        ?politicianUri schema:familyName ?pFamilyName .
+        BIND(CONCAT(?pGivenName, " ", ?pFamilyName) AS ?politician) .
+
+        ?interests a lw:MembersInterest .
+        ?interests lw:parliamentMember ?politicianUri.
+        ?interests org:organization ?orgUri .
+        ?orgUri schema:name ?org .
+
+        FILTER(LANG(?org) = "de")
+        } ORDER BY ?pFamilyName
+    """
+
+    df = client.send_query(query)
+    rows_to_take = (df["org"].value_counts() > 10) & (df["org"].value_counts() < 20)
+    relevant_orgs = set(rows_to_take[rows_to_take].index)
+    df = df[df['org'].isin(relevant_orgs)]
+
+    elements = []
+    for iterator, group in zip([set(df["org"]), df["politician"]], ["org", "politician"]):
+        for node in iterator:
+            item = {
+                "data" : {
+                    "id": node,
+                    "label": node
+                    },
+                "classes": group
+            }
+            elements.append(item)
+
+    for source, target in zip(df["org"], df["politician"]):
+        item = {
+            "data": {
+                "source": source,
+                "target": target
+            }
         }
-    }
-    elements.append(item)
+        elements.append(item)
+
+    return elements
+
 
 cyto.load_extra_layouts()
+
+elements = []
+elements += generate_politician_party_relation()
+elements += generate_politician_org_relation()
 
 default_stylesheet = [
     {
@@ -67,6 +129,11 @@ default_stylesheet = [
         'style': {
             'background-color': 'grey'}
     },
+    {
+        'selector': '.org',
+        'style': {
+            'background-color': 'yellow'}
+    }
 ]
 app = dash.Dash(__name__)
 
@@ -75,8 +142,9 @@ app.layout = html.Div([
     cyto.Cytoscape(
         id='cytoscape',
         elements=elements,
-        layout={'name': 'cose', 'animate': False}, #klay, cola
+        layout={'name': 'cose', 'animate': False}, #cose, klay, cola
         style={'width': '1800px', 'height': '1000px'},
+        #stylesheet=default_stylesheet,
     )
 ])
 
@@ -93,6 +161,15 @@ def generate_stylesheet(node):
             'background-color': 'blue',
             'label': 'data(label)',
             'z-index': 9998}
+    },{
+        'selector': '.politician',
+        'style': {
+            'background-color': 'grey'}
+    },
+    {
+        'selector': '.org',
+        'style': {
+            'background-color': 'yellow'}
     },{
         "selector": 'node[id = "{}"]'.format(node['data']['id']),
         'style': {
